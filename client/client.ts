0190edab697f3dca71f969eb910af2c6206df663
marpnz <mariajosepomz1@gmail.com>
2026-03-09 12:03:58 -0600

    Update client.ts

--- client/client.ts
+++ client/client.ts
@@ -1,4 +1,34 @@
-// Client
-console.log("My address:", pg.wallet.publicKey.toString());
-const balance = await pg.connection.getBalance(pg.wallet.publicKey);
-console.log(`My balance: ${balance / web3.LAMPORTS_PER_SOL} SOL`);
+// No imports needed: web3, anchor, pg and more are globally available
+
+describe("Test", () => {
+  it("initialize", async () => {
+    // Generate keypair for the new account
+    const newAccountKp = new web3.Keypair();
+
+    // Send transaction
+    const data = new BN(42);
+    const txHash = await pg.program.methods
+      .initialize(data)
+      .accounts({
+        newAccount: newAccountKp.publicKey,
+        signer: pg.wallet.publicKey,
+        systemProgram: web3.SystemProgram.programId,
+      })
+      .signers([newAccountKp])
+      .rpc();
+    console.log(`Use 'solana confirm -v ${txHash}' to see the logs`);
+
+    // Confirm transaction
+    await pg.connection.confirmTransaction(txHash);
+
+    // Fetch the created account
+    const newAccount = await pg.program.account.newAccount.fetch(
+      newAccountKp.publicKey
+    );
+
+    console.log("On-chain data is:", newAccount.data.toString());
+
+    // Check whether the data on-chain is equal to local 'data'
+    assert(data.eq(newAccount.data));
+  });
+});
